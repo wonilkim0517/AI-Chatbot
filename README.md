95c2f849847dd219e00fe705325cc2c1b8b2cfab
WonJin Hong <own1045@gmail.com>
2024-05-21 15:53:20 +0900

    merge codes

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # 삼육대학교 SW캡스톤 프로젝트
 
-26조
 
+
+# run
 ```pwsh
 streamlit run streamlit_main.py
+```
+
+
+### pykospacing 설치
+
+```pwsh
+pip install git+https://github.com/haven-jeon/PyKoSpacing.git
 ```
--- streamlit_main.py
+++ streamlit_main.py
@@ -1,71 +1,141 @@
+import os
+import re
+import shutil
 import webbrowser
+import unicodedata
 
+import numpy as np
 import streamlit as st
-import tiktoken
-from loguru import logger
 
-from langchain.chains import ConversationalRetrievalChain
-from langchain.chat_models import ChatOpenAI
+from pykospacing import Spacing
+from transformers import BertTokenizer
 
-from langchain.document_loaders import PyPDFLoader
-from langchain.document_loaders import Docx2txtLoader
-from langchain.document_loaders import UnstructuredPowerPointLoader
-
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
+from langchain_community.chat_models import ChatOpenAI  # 버전에 맞춰 다르게 임포트
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import Docx2txtLoader
+from langchain_community.document_loaders import UnstructuredPowerPointLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings import HuggingFaceEmbeddings
-
+from langchain_text_splitters import CharacterTextSplitter
 from langchain.memory import ConversationBufferMemory
-from langchain.vectorstores import FAISS
-
-# from streamlit_chat import message
-from langchain.callbacks import get_openai_callback
 from langchain.memory import StreamlitChatMessageHistory
+from langchain.callbacks import get_openai_callback
+from langchain.prompts import PromptTemplate
+
+from sentence_transformers import SentenceTransformer, util
+
+# from utils.functions import (
+#     get_pdf_files,
+#     remove_underscore_after_pdf,
+#     chg_itemname,
+#     copy_files_to_another_folder,
+#     correct_spacing,
+#     complete_sentence,
+#     length_based_filtering,
+# ) -> streamlit은  'st.set_page_config'가 무조건 먼저 실행되어야해서 이러면 오류났음
 
 
-def tiktoken_len(text):
-    tokenizer = tiktoken.get_encoding("cl100k_base")
-    tokens = tokenizer.encode(text)
-    return len(tokens)
+# 디렉토리 경로로 파일을 가져오기
+def get_pdf_files(directory):
+    file_list = os.listdir(directory)
+    pdf_files = [file for file in file_list if file.endswith(".pdf")]
+    return pdf_files
 
 
-def get_text(docs):
-    doc_list = []
+# ?
+def remove_underscore_after_pdf(pdf_files):
+    modified_pdf_files = []
 
-    for doc in docs:
-        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
-        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
-            file.write(doc.getvalue())
-            logger.info(f"Uploaded {file_name}")
-        if ".pdf" in doc.name:
-            loader = PyPDFLoader(file_name)
-            documents = loader.load_and_split()
-        elif ".docx" in doc.name:
-            loader = Docx2txtLoader(file_name)
-            documents = loader.load_and_split()
-        elif ".pptx" in doc.name:
-            loader = UnstructuredPowerPointLoader(file_name)
-            documents = loader.load_and_split()
+    for file in pdf_files:
+        base_name = os.path.splitext(file)[0]
+        base_name_without_underscore = base_name.split("_")[0]
+        new_file_name = f" {base_name_without_underscore}"
+        modified_pdf_files.append(new_file_name)
 
-        doc_list.extend(documents)
-    return doc_list
+    return modified_pdf_files
 
 
-def get_text_chunks(text):
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=900, chunk_overlap=100, length_function=tiktoken_len
-    )
-    chunks = text_splitter.split_documents(text)
-    return chunks
+# 한글 자모 깨짐 방지
+def chg_itemname(itemnames):
+    itemlists = list()
+    for itemname in itemnames:
+        itemlists.append(unicodedata.normalize("NFC", itemname))
+    return itemlists
 
 
-def get_vectorstore(text_chunks):
-    embeddings = HuggingFaceEmbeddings(
-        model_name="jhgan/ko-sroberta-multitask",
-        model_kwargs={"device": "cpu"},
-        encode_kwargs={"normalize_embeddings": True},
-    )
-    vectordb = FAISS.from_documents(text_chunks, embeddings)
-    return vectordb
+# 가져온 파일을 DOC 폴더로 넣기
+def copy_files_to_another_folder(
+    source_folder, target_folder, files_to_copy
+):  # 대상 폴더가 없으면 생성 / 폴더가 있으면 내용 비우기
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder, exist_ok=True)
+        print('create "docs" folder')
+    else:
+        for f in os.listdir(target_folder):
+            os.remove(os.path.join(target_folder, f))
+        print("clear folder")
+
+    for file_to_copy in files_to_copy:
+        source_path = os.path.join(source_folder, file_to_copy)
+        target_path = os.path.join(target_folder, file_to_copy)
+
+        try:
+            shutil.copy2(source_path, target_path)
+            print(f"File '{file_to_copy}' copied successfully.")
+        except FileNotFoundError:
+            print(f"File '{file_to_copy}' not found in the source folder.")
+
+
+# BERT 토크나이저를 이용한 한글 토큰 추출 함수
+def extract_korean_tokens(text):
+    tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
+    tokens = tokenizer.tokenize(text)
+    korean_tokens = [token for token in tokens if re.match("^[가-힣]+$", token)]
+    return korean_tokens
+
+
+# 띄어쓰기 오류 교정 함수
+def correct_spacing(text):
+    spacing = Spacing()
+    corrected_text = spacing(text)
+    return corrected_text
+
+
+# 불완전 문장 처리 함수
+def complete_sentence(text):
+    # 문장의 끝에 마침표가 없으면 추가
+    print("text: ", text)
+    if text[-1] not in [".", "?", "!"]:
+        text += "."
+    return text
+
+
+# 길이 기반 필터링 함수
+def length_based_filtering(text, min_length=10):
+    if len(text) > min_length:
+        return text
+    else:
+        return ""
+
+
+# BERT 임베딩을 사용하여 전처리 및 필터링을 수행하는 함수
+def preprocess_with_bert_embedding(text):
+    # 한글 토큰 추출
+    korean_tokens = extract_korean_tokens(text)
+
+    # 띄어쓰기 오류 교정
+    corrected_text = correct_spacing(" ".join(korean_tokens))
+
+    # 불완전 문장 처리
+    completed_text = complete_sentence(corrected_text)
+
+    # 길이 기반 필터링
+    filtered_text = length_based_filtering(completed_text)
+
+    return filtered_text
 
 
 # 사이드 바의 LLM 옵션 받기 위해 수정
@@ -90,8 +160,8 @@ def get_conversation_chain(vetorestore, openai_api_key, llm_option_1, llm_option
     return conversation_chain
 
 
-def main():
-    st.set_page_config(page_title="~AI 챗봇", page_icon=":books:")
+def main(origin_path, copy_path, top_k):
+    st.set_page_config(page_title="AI 챗봇")
 
     st.title("_개인 데이터 :red[QA 채팅]_ :books:")
 
@@ -113,9 +183,23 @@ def main():
         sidebar_tab1, sidebar_tab2 = st.tabs(["기본 탭", "추가 탭"])
 
         with sidebar_tab1:
-            uploaded_files = st.file_uploader(
-                "파일을 업로드하세요.", type=["pdf", "docx"], accept_multiple_files=True
+            upload_option = st.radio(
+                "파일을 업로드할 방법 선택하세요", ("파일 선택", "경로 입력")
             )
+
+            if upload_option == "파일 선택":
+                uploaded_files = st.file_uploader(
+                    "파일을 업로드하세요.",
+                    type=["pdf", "docx", "pptx"],
+                    accept_multiple_files=True,
+                )
+            elif upload_option == "경로 입력":
+                directory_path = st.text_input("디렉토리 경로를 입력하세요.")
+                if os.path.isdir(directory_path):
+                    uploaded_files = get_pdf_files(directory_path)
+                else:
+                    st.warning("유효한 디렉토리 경로를 입력하세요.")
+                    uploaded_files = None
 
             st.divider()
 
@@ -123,6 +207,7 @@ def main():
                 "OpenAI API Key를 입력하세요", key="chatbot_api_key", type="password"
             )
             process = st.button("OpenAI API Key 적용")
+            # print(process)
 
             st.divider()
 
@@ -167,20 +252,23 @@ def main():
             st.header("추가 탭 1")
             st.markdown("#### 아직 기능을 구현하는 중입니다")
 
+    embedder = SentenceTransformer(
+        "jhgan/ko-sroberta-multitask"
+    )  # 한글을 잘 가져오는 거?
     if process:
         if not openai_api_key:
             st.info(
                 "계속하려면 OpenAI API 키를 추가하세요."
             )  # Please add your OpenAI API key to continue.
             st.stop()
-        files_text = get_text(uploaded_files)
-        text_chunks = get_text_chunks(files_text)
-        vetorestore = get_vectorstore(text_chunks)
 
-        st.session_state.conversation = get_conversation_chain(
-            vetorestore, openai_api_key
-        )
-        st.session_state.processComplete = True
+        modified_files = remove_underscore_after_pdf(uploaded_files)
+        pdf_files_name = chg_itemname(modified_files)
+
+        filename_embeddings = embedder.encode(pdf_files_name, convert_to_tensor=True)
+
+        # filename_embeddings을 if query~에서도 사용하기 위해 session_state 사용
+        st.session_state["embedded_filename"] = filename_embeddings
 
     if "messages" not in st.session_state:
         st.session_state["messages"] = [
@@ -196,15 +284,80 @@ def main():
 
     history = StreamlitChatMessageHistory(key="chat_messages")
 
-    # if llm_option1_slider:
-    #     pass
-
-    # if llm_option2_slider:
-    #     pass
-
     # Chat logic
     if query := st.chat_input("질문을 입력해주세요."):
+        print("query", query)
+
+        # template = """ 당신은 의학에 대해 대답하는 챗봇입니다. 주어진 문서에 있는 내용으로 대답하시오.
+        # 만약 문서에 없는 내용이면 문서에 존재하지 않는다고 답변하세요. 모른다고 지어내서 말하지 말하세요.
+        # File Name : 을 참조해서 어떤 절차에 대한 문서인지 파악하고 질문과 동일한 절차의 내용만 사용하세요.
+        # {context}
+        # 질문 : {query}
+        # 너의 답변 : """
+
+        # prompt = PromptTemplate(input_variables= ["context", "query"], template=template) -> !TODO prompt.메소드로 넣어야함 수정필요
+
+        embedded_filename = st.session_state["embedded_filename"]
+
+        # 질문과 유사한 파일을 고르고, 고른 파일만 가져옴
+        question_embedding = embedder.encode(query, convert_to_tensor=True)
+        # print(type(question_embedding), 'q')
+        cos_scores = util.pytorch_cos_sim(question_embedding, embedded_filename).cpu()
+        top_results = np.argpartition(-cos_scores, range(top_k))[
+            0:top_k
+        ]  # !TODO 단일 문서일 경우 예외처리는?
+
+        files_to_copy = []
+        for idx in top_results[0][0:top_k]:
+            files_to_copy.append(uploaded_files[idx])
+
+        copy_files_to_another_folder(origin_path, copy_path, files_to_copy)
+
+        loader = PyPDFDirectoryLoader(copy_path)
+        documents = loader.load()
+
+        cleaned_documents = []
+        for document in documents:
+            cleaned_document = document  # metadata에서 파일명을 추출합니다.
+            source_path = document.metadata.get("source", "")
+            file_name = source_path.split("docs/")[-1]
+            file_name = file_name.split("_")[0]
+            file_name = unicodedata.normalize("NFC", file_name)
+
+            page_content = document.page_content
+
+            cleaned_text = f"File Name: {file_name} \n"
+
+            # cleaned_text = preprocess_with_bert_embedding(page_content)  <- !TODO 사용시 정확도가 너무 낮음
+            cleaned_text = page_content
+            cleaned_document.page_content = cleaned_text
+            cleaned_documents.append(cleaned_document)
+
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000, chunk_overlap=200, add_start_index=True
+        )
+        all_splits = text_splitter.split_documents(cleaned_documents)
+
+        embeddings = HuggingFaceEmbeddings(
+            model_name="jhgan/ko-sroberta-multitask",
+            encode_kwargs={"normalize_embeddings": True},
+        )
+
+        vector_store = FAISS.from_documents(
+            # vectorstore = Chroma.from_documents(
+            documents=all_splits,
+            embedding=embeddings,
+        )
+
+        # 질문 받고 한글 전처리하고 모델 생성
+        print("modeling ... ")
+        st.session_state.conversation = get_conversation_chain(
+            vector_store, openai_api_key, llm_option1, llm_option2
+        )
+        st.session_state.processComplete = True
+
         st.session_state.messages.append({"role": "user", "content": query})
+        print("done !")
 
         with st.chat_message("user"):
             st.markdown(query)
@@ -225,17 +378,31 @@ def main():
                         source_documents[0].metadata["source"],
                         help=source_documents[0].page_content,
                     )
-                    st.markdown(
-                        source_documents[1].metadata["source"],
-                        help=source_documents[1].page_content,
-                    )
-                    st.markdown(
-                        source_documents[2].metadata["source"],
-                        help=source_documents[2].page_content,
-                    )
+                    # st.markdown(
+                    #     source_documents[1].metadata["source"],
+                    #     help=source_documents[1].page_content,
+                    # )
+                    # st.markdown(
+                    #     source_documents[2].metadata["source"],
+                    #     help=source_documents[2].page_content,
+                    # ) -> !TODO 참고문서 수를 1개로 줄였으니 1개만 나둠. top_k에 따른 예외처리 필요
+
         # Add assistant message to chat history
         st.session_state.messages.append({"role": "assistant", "content": response})
 
 
 if __name__ == "__main__":
-    main()
+    top_k = 1  # 가져올 문서의 개수 설정
+    DATA_PATH = "./datas/"  # 원본 파일 위치
+    DOC_PATH = "./docs/"  # 복사한 파일 위치 (유사도순으로 추출 후)
+
+    main(DATA_PATH, DOC_PATH, top_k)
+    # BERT 토크나이저 로드
+    # tokenizer = BertTokenizer.from_pretrained("bert-base-multilingual-cased")
+
+# Q
+# .doc 확장자는 어떻게?
+
+# !TODO
+# 템플릿으로 입력받기
+# 파일 선택으로 1개 업로드시 예외처리
